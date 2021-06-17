9983ddb475189ce4141f5626f8ad7857e78b9392
Lautarobentancor <79489537+Lautarobentancor@users.noreply.github.com>
2021-06-16 23:21:50 -0300

    arreglos

--- src/components/HowToFetchData.js
+++ src/components/HowToFetchData.js
@@ -6,25 +6,42 @@ function HowToFetchData() {
     fetch("https://fakestoreapi.com/products?limit=4")
       .then((res) => res.json())
       .then((res) => {
-        setData(res.results);
+        setData(res);
       });
   }, []);
-  return (
+  console.log(data);
+   return ( 
     <div>
-      {data !== null ? (
+        {data !== null ? (
         data.map((data, index) => {
-            return(
-              <div>
-                <h1>{data.id}</h1>
-                <h2>{data.title}</h2>
-              </div>
-            )
-        })
-      ) : (
-        <h3>Cargada</h3>
-      )}
-    </div>
-  );
+              return(
+                 <div>
+                  <h1>{data.id}</h1>
+                  <h2>{data.description}</h2>
+                  <img src={data.image}></img>
+                </div>
+               )
+           })
+         ) : (
+           <h3>Cargada</h3>
+        )}
+       </div>
+   )
+  // //   <div>
+  // //     {data !== null ? (
+  // //       data.map((data, index) => {
+  // //           return(
+  // //             <div>
+  // //               <h1>{data.id}</h1>
+  // //               <h2>{data.title}</h2>
+  // //             </div>
+  // //           )
+  // //       })
+  // //     ) : (
+  // //       <h3>Cargada</h3>
+  // //     )}
+  // //   </div>
+  // // );
 }
 
 export default HowToFetchData;
